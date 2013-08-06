05d21e1dd90c0b14a71c615c14911416180055ca
gskielian <gregory.kielian@hackerdojo.com>
2013-08-06 14:06:32 -0700

    Create PanTilt.ino

---
+++ Motors/PanTilt.ino
@@ -0,0 +1,34 @@
+#include <Servo.h> 
+
+Servo servz;
+
+int command = 0;
+
+
+void setup()
+{
+  servz.attach(9);
+
+  Serial.begin(9600);  
+
+}
+
+void loop()
+{
+  if(Serial.available())
+  {
+      command = Serial.parseInt();
+      if (command  == 1 )
+      {
+      }
+      if (command == 0)
+      {
+      }
+  }
+}
+
+void scan()
+{
+  
+  
+}

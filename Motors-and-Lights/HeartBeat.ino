ccba104337f06280a48e2300ff61ba163ea1f87d
gskielian <gregory.kielian@hackerdojo.com>
2013-08-05 22:02:49 -0700

    Create HeartBeat.ino

---
+++ Motors-and-Lights/HeartBeat.ino
@@ -0,0 +1,29 @@
+void setup()
+{
+ pinMode(9,OUTPUT); //This allows 13 to move a lot of electricity (a lot of amps)
+}
+
+
+void loop()
+{
+fadeOut(10);
+fadeIn(10);
+}
+
+void fadeOut(int x)
+{
+    for(int i = 255; i >= 0 ; i-- )
+    {
+      analogWrite(9,i); //always on
+      delay(x);
+    }   
+}
+
+void fadeIn(int x)
+{
+   for(int i = 0; i <= 255 ; i++ )
+    {
+      analogWrite(9,i); //always on
+      delay(x);
+    }  
+}
